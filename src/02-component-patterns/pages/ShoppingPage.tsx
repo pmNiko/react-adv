b110ec57d0ce2c74debb8d0b2c8a512d215441c3
pmNiko <nikolas090189@gmail.com>
2022-03-03 19:26:44 -0300

    finish npm

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,6 +1,5 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components/'
 import products from '../data/product'
-import '../styles/custom-styles.css'
 
 const product = products[0]
 
@@ -13,7 +12,6 @@ export const ShoppingPage = () => {
             <ProductCard
                 key={product.id}
                 product={product}
-                className="bg-dark"
                 initialValues={{
                     count: 4,
                     maxCount: 10,
@@ -21,29 +19,10 @@ export const ShoppingPage = () => {
             >
                 {({ count, maxCount, isMaxCountReached, increaseBy, reset }): JSX.Element => (
                     <>
-                        <ProductImage
-                            className="custom-image"
-                            style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
-                        />
+                        <ProductImage />
 
-                        <ProductTitle className="text-white text-bold" />
-                        <ProductButtons className="custom-buttons" />
-                        <button onClick={reset}>Volver a estado inicial</button>
-                        <button onClick={() => increaseBy(-2)}>-2</button>
-
-                        {isMaxCountReached ? (
-                            <>
-                                <br />
-                                <span style={{ color: 'red' }}>
-                                    Ha llegado a la cantidad maxima!
-                                </span>
-                            </>
-                        ) : (
-                            <button onClick={() => increaseBy(+2)}>+2</button>
-                        )}
-                        <span style={{ color: 'white' }}>
-                            {count} - {maxCount}
-                        </span>
+                        <ProductTitle />
+                        <ProductButtons />
                     </>
                 )}
             </ProductCard>
